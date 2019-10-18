aea354ba8e16ebd98b42bbd946fd4f8ff5f794de
Eddie Potocko <epotocko@gmail.com>
2019-10-18 22:17:08 +0000

    Adding more unit tests

--- test/test_spellchecker.js
+++ test/test_spellchecker.js
@@ -19,6 +19,23 @@ describe('SpellChecker', function() {
 				done();
 			});
 		});
+		it('detects multiple correctly spelled words async', function(done) {
+			let c = new SpellChecker();
+			let words = ['table', 'tble', 'fish', 'fosj', 'apples', 'aples', 'truth', 'treth'];
+			let expectedResult = [false, true, false, true, false, true, false, true];
+			let promises = [];
+			for(let word of words) {
+				promises.push(new Promise((resolve, reject) => { 
+					c.isMisspelledAsync(word, function(err, misspelled) {
+						err ? reject(err) : resolve(misspelled);
+					});
+				}));
+			}
+			Promise.all(promises).then((results) => {
+				assert.deepStrictEqual(results, expectedResult);
+				done();
+			});
+		});
 	});
 	describe("#getCorrectionsForMisspelling()", function() {
 		it('describe suggestions for misspellings', function() {
@@ -27,5 +44,35 @@ describe('SpellChecker', function() {
 			assert.equal(suggestions.length > 5, true);
 			assert.equal(suggestions.includes('pencils'), true);
 		});
-	})
+	});
+	describe("#getCorrectionsForMisspellingAsync()", function() {
+		it('describe suggestions for misspellings async', function(done) {
+			let c = new SpellChecker();
+			c.getCorrectionsForMisspellingAsync("pancils", function(err, suggestions) {
+				assert.equal(err, undefined);
+				assert.equal(suggestions.length > 5, true);
+				assert.equal(suggestions.includes('pencils'), true);
+				done();	
+			});
+		});
+		it('describe multiple suggestions for misspellings async', function(done) {
+			let c = new SpellChecker();
+			let words = ['tble', 'bateries', 'fost', 'aples', 'treth'];
+			let corrections = ['table', 'batteries', 'fast', 'apples', 'truth']
+			let promises = [];
+			for(let word of words) {
+				promises.push(new Promise((resolve, reject) => { 
+					c.getCorrectionsForMisspellingAsync(word, (err, misspelled) => {
+						err ? reject(err) : resolve(misspelled);
+					});
+				}));
+			}
+			Promise.all(promises).then((results) => {
+				for(var i = 0; i < corrections.length; i++) {
+					assert.equal(results[i].includes(corrections[i]), true);
+				}
+				done();
+			});
+		});
+	});
 });
