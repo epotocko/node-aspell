b606887d3a382d178b7d50ad820b6b2338c2e02d
Eddie Potocko <epotocko@gmail.com>
2019-10-25 18:29:48 +0000

    Cleaning up indentation

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "SpellChecker.h"
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return SpellChecker::Init(env, exports);
+	return SpellChecker::Init(env, exports);
 }
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, InitAll)
